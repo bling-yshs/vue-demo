4cbe26adcaf7741f3ed8d42b939a0f705f176446
bling-yshs <377178599@qq.com>
2025-04-22 18:55:25 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('@/views/home/home-index.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/home/home-index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="text-green-300">hello world</div>
+    <Button class="active:scale-95" @click="hi">Hello</Button>
+    <Input v-model="obj.name"></Input>
+    <div>{{ obj.name }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { ref } from 'vue'
+
+interface Obj {
+  id: string
+  name: string
+}
+
+const obj = ref<Obj>({ id: '', name: '' })
+
+async function hi() {
+  console.log(obj.value)
+  console.log(await (await fetch('https://xiaohai.ystech.top/test')).json())
+}
+</script>
