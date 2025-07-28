8b3d453995bdc80710908716efbe91417342d1ca
bling-yshs <377178599@qq.com>
2025-07-28 10:26:33 +0800

    rolldown

--- src/views/home/home-index.vue
+++ src/views/home/home-index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Input></Input>
     <div class="text-orange-500">hello world</div>
     <Button class="active:scale-95" @click="hi">这是一个测试按钮哦~</Button>
     <Input v-model="obj.name"></Input>
@@ -21,7 +20,8 @@ interface Obj {
 const obj = ref<Obj>({ id: '', name: '' })
 
 async function hi() {
-  console.log(obj.value)
-  console.log(await (await fetch('https://xiaohai.ystech.top/test')).json())
+  let response = await fetch('\n' +
+    'https://api.vc.bilibili.com/link_setting/v1/link_setting/get?build=0&mobi_app=web&msg_notify=1&show_unfollowed_msg=1&web_location=333.1007')
+  console.log((await response.text()))
 }
 </script>
