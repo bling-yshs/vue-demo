5fc9fc273b7965e276c7cfe484db320d945347d5
bling-yshs <377178599@qq.com>
2025-09-12 15:05:36 +0800

    temp

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-## 更新
+---
+type: "always_apply"
+---
 
-```shell
-pnpm outdated
-```
+# 总体架构
+
+- Vue3 前端项目
+
+## 前端框架
+
+- 使用 vue3
+- 使用 typescript
+- 使用 pnpm 包管理器，任何 npm 命令都要换成 pnpm 命令
+- 使用 tailwind v4,shadcn-vue,vue-router
+- 使用 fetch 进行网络请求
+
+# UI组件规范
+
+- 只能使用 shadcn-vue 的组件，在没有允许的情况下，不允许自己新建任何组件
+- 缺少 shadcn-vue 的组件时，可以执行命令 pnpm dlx shadcn-vue@latest add [component] 来安装
+
+# 编码规范
+
+- 不允许编写任何测试
+- 编写完代码以后进行验证，我会自己测试
+- 在每次修改结束以后，都需要运行 pnpm biome check --write ./src 命令进行格式化
+- 前端vue的script里的函数不要用带变量的箭头函数，尽量使用命名函数
+- 所有的日志都使用中文
--- src/views/home/home-index.vue
+++ src/views/home/home-index.vue
@@ -2,30 +2,19 @@
   <div>
     <div class="text-orange-500">hello world</div>
     <Button class="active:scale-95" @click="hi">这是一个测试按钮哦~</Button>
-    <Input v-model="obj.name"></Input>
-    <div>{{ obj.name }}</div>
+    <Input v-model="inp"></Input>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-// biome-ignore lint/correctness/noUnusedImports: a
+
+const inp = ref('')
+
 import { Button } from '@/components/ui/button'
-// biome-ignore lint/correctness/noUnusedImports: a
 import { Input } from '@/components/ui/input'
 
-interface Obj {
-  id: string
-  name: string
-}
-
-const _obj = ref<Obj>({ id: '', name: '' })
-
-async function _hi() {
-  let response = await fetch(
-    '\n' +
-      'https://api.vc.bilibili.com/link_setting/v1/link_setting/get?build=0&mobi_app=web&msg_notify=1&show_unfollowed_msg=1&web_location=333.1007',
-  )
-  console.log(await response.text())
+async function hi() {
+  console.log('ok')
 }
 </script>
