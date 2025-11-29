2dd327cf10f93e8e4c576b5241661dab012ed991
bling-yshs <377178599@qq.com>
2025-11-29 19:02:46 +0800

    feat: 添加 vue-draggable-plus 和 uuid 依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import tailwindcss from '@tailwindcss/vite'
-import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), legacy({
-
-  })],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
